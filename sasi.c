89e9e91003d26d40c207152d7f1c90d701ed6b62
sasiisas <30343437+sasiisas@users.noreply.github.com>
2017-07-22 17:03:02 +0530

    Create sasi.c

---
+++ sasi.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n;
+printf("enter the number:");
+scanf("%d",&n);
+if(n/2=0)
+{
+printf("\nGiven no %d is even",n);
+}
+else
+{
+printf("\nGiven no %d is odd",n);
+}
+getch();
+}
